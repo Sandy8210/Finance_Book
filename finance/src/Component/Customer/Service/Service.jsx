b07eda59398e7191e48165fcf5457f2634945f4d
KGM <KGM@CASL6-22-150.kgisledu.com>
2024-05-03 11:54:58 +0530

    copy

--- finance/src/Component/Customer/Service/Service.jsx
+++ finance/src/Component/Customer/Service/Service.jsx
@@ -2,14 +2,125 @@ import React from 'react'
 
 const Service = () => {
   return (
+    <>
+     <h3 className='serviceheading'>Our Service</h3>
     <div className='servicecontainer'>
-      <div className="card" style={{ width: '18rem' }}>
-        <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '290px' }} />
+     <div className='container col-lg-12 col-md-9 '>
+    
+      <div className='row '>
+
+      <div className='col-lg-4 col-md-3'>
+      <div className="card " style={{ width: '18rem' }}>
+        <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
         <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 className="card-title">Insurance Consulting</h5>
+          <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
         </div>
       </div>
+      </div>
+      <div className='col-lg-4 col-md-3'>
+      <div className="card  " style={{ width: '18rem' }}>
+        <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+        <div className="card-body">
+        <h5 className="card-title">Insurance Consulting</h5>
+          <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+        </div>
+      </div>
+      </div>
+      <div className='col-lg-4 col-md-3'>
+      <div className="card  " style={{ width: '18rem' }}>
+        <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+        <div className="card-body">
+        <h5 className="card-title">Insurance Consulting</h5>
+          <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+        </div>
+      </div>
+      </div>
+
+      
+      </div>
+
+
     </div>
+    <div className='container mt-3 col-lg-12 col-md-9 '>
+    
+    <div className='row '>
+
+    <div className='col-lg-4 col-md-3'>
+    <div className="card " style={{ width: '18rem' }}>
+      <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+      <div className="card-body">
+      <h5 className="card-title">Insurance Consulting</h5>
+        <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+      </div>
+    </div>
+    </div>
+    <div className='col-lg-4 col-md-3'>
+    <div className="card  " style={{ width: '18rem' }}>
+      <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+      <div className="card-body">
+      <h5 className="card-title">Insurance Consulting</h5>
+        <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+      </div>
+    </div>
+    </div>
+    <div className='col-lg-4 col-md-3'>
+    <div className="card  " style={{ width: '18rem' }}>
+      <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+      <div className="card-body">
+      <h5 className="card-title">Insurance Consulting</h5>
+        <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+      </div>
+    </div>
+    </div>
+
+    
+    </div>
+
+
+  </div>
+  <div className='container mt-3 col-lg-12 col-md-9 '>
+    
+    <div className='row '>
+
+    <div className='col-lg-4 col-md-3'>
+    <div className="card " style={{ width: '18rem' }}>
+      <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+      <div className="card-body">
+      <h5 className="card-title">Insurance Consulting</h5>
+        <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+      </div>
+    </div>
+    </div>
+    <div className='col-lg-4 col-md-3'>
+    <div className="card  " style={{ width: '18rem' }}>
+      <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+      <div className="card-body">
+      <h5 className="card-title">Insurance Consulting</h5>
+        <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+      </div>
+    </div>
+    </div>
+    <div className='col-lg-4 col-md-3'>
+    <div className="card  " style={{ width: '18rem' }}>
+      <img src="https://templates.thememodern.com/finance/images/Services/3.jpg" className="card-img-top" alt="" style={{ width: '285px' }} />
+      <div className="card-body">
+      <h5 className="card-title">Insurance Consulting</h5>
+        <p className="card-text">  Provide recommendations on the types and levels of insurance coverage required to mitigate risks and protect against potential losses.</p>
+      </div>
+    </div>
+    </div>
+
+    
+    </div>
+
+
+  </div>
+    
+
+
+    </div>
+    </>
 
   )
 }
